25601ee73c77bf33f28f21fda49455376c74b254
soul0101 <53980340+soul0101@users.noreply.github.com>
2022-02-03 16:53:52 +0530

    Bug fix

--- src/components/Banks/VerifyClient.jsx
+++ src/components/Banks/VerifyClient.jsx
@@ -1,43 +1,35 @@
-import React, { useEffect,useState} from 'react';
-import ClientData from"../Client/ClientData";
+import React, { useEffect, useState } from "react";
+import ClientData from "../Client/ClientData";
 
-const VerifyClient = ({data,dmr,accounts}) => {
-
+const VerifyClient = ({ data, dmr, accounts }) => {
   const [clientData, setClientData] = useState(null);
   useEffect(() => {
-
     if (dmr && accounts) {
       dmr.methods
         .getCustomerDetails(data.kycId)
         .call({ from: accounts[0] })
         .then((res) => {
-          console.log(res)
+          console.log(res);
           let t = {
-            name:res.name,
-            gender:res.gender,
-            phone:res.phone,
-            address:res.customerAddress,
-            email:res.email,
-            kycId:res.kycId,
-            kycStatus:res.kycStatus,
-            records:res.records,
-            kycHistory:res.kycHistory
-          }
-          setClientData(t);          
+            name: res.name,
+            gender: res.gender,
+            phone: res.phone,
+            address: res.customerAddress,
+            kycId: res.kycId,
+            kycStatus: res.kycStatus,
+            records: res.records,
+            kycHistory: res.kycHistory,
+          };
+          setClientData(t);
         })
         .catch((err) => {
           console.log(err);
         });
-    }    
-     // eslint-disable-next-line         
+    }
+    // eslint-disable-next-line
   }, []);
 
-  
-  return (
-    <>     
-      {clientData && <ClientData userData={clientData}/>}            
-    </>
-  )
+  return <>{clientData && <ClientData userData={clientData} />}</>;
 };
 
 export default VerifyClient;
